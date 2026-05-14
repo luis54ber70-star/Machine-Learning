75e0c584d0cb2c10a85160fbd0385a63de821fe3
luis54ber70-star <luis54ber.70@gmail.com>
2026-05-14 03:41:32 -0600

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+def calculate_form(points):
+
+    if points >= 12:
+        return "Excellent"
+
+    elif points >= 8:
+        return "Good"
+
+    elif points >= 5:
+        return "Average"
+
+    return "Bad"
